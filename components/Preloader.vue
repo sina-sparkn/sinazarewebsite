ea669e3706594d43fe37d35e15857ead0068f893
ssparknt <sina.hakim.red@gmail.com>
2024-12-03 20:16:46 +0200

    added initial animation

---
+++ components/Preloader.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="preloader" v-if="loading">
+    <div class="spinner"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    duration: {
+      type: Number,
+      default: 2000, // 2 seconds
+    },
+  },
+  data() {
+    return {
+      loading: true,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.loading = false;
+    }, this.duration);
+  },
+};
+</script>
+
+<style>
+.preloader {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #121212;
+  z-index: 9999;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :class="{ 'fade-in': showContent }">
+    <NuxtPage />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showContent: false,
+    };
+  },
+  mounted() {
+    // Delay to simulate loading and then show content
+    setTimeout(() => {
+      this.showContent = true;
+    }, 0); // Adjust the delay as needed
+  },
+};
+</script>
+
+<style>
+@import "~/assets/css/animations.css";
+</style>
